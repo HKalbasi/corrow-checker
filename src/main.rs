31457419d681ac7e1b021d6ea88cbbb865bc4a2c
hkalbasi <hamidrezakalbasi@protonmail.com>
2023-11-05 01:02:24 +0330

    Start using ariadne

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::error;
+use std::{error, fs::read_to_string};
 
+use ariadne::{Label, Report, Source};
 use lang_c::{
     ast::{DeclaratorKind, ExternalDeclaration, Identifier},
     driver::{parse, Config},
@@ -9,8 +10,8 @@ use lower::lower_body;
 use crate::check::check_cfg;
 
 mod cfg;
-mod lower;
 mod check;
+mod lower;
 
 fn main() {
     let config = Config::default();
@@ -20,7 +21,15 @@ fn main() {
             ExternalDeclaration::Declaration(decl) => {
                 for decls in &decl.node.declarators {
                     if let DeclaratorKind::Identifier(id) = &decls.node.declarator.node.kind.node {
-                        println!("decl {}", id.node.name);
+                        Report::build(ariadne::ReportKind::Advice, "malloc_leak1.c", 0)
+                            .with_message(format!("there is a decl here named {}", id.node.name))
+                            .with_label(
+                                Label::new(("malloc_leak1.c", id.span.start..id.span.end))
+                                    .with_message("it is here")
+                                    .with_color(ariadne::Color::Cyan),
+                            )
+                            .finish()
+                            .eprint(("malloc_leak1.c", Source::from(&ast.source)));
                     }
                 }
             }
